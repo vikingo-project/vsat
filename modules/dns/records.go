4addfd48680c0e1c1bac61ed188fd528c2963972
Igor K <akamajoris@gmail.com>
2021-10-06 02:16:17 +0300

    Added CAA records to DNS module

--- modules/dns/records.go
+++ modules/dns/records.go
@@ -9,6 +9,9 @@ type Record struct {
 	Type    string `json:"type"`
 	Name    string `json:"name"`
 	Content string `json:"content"`
+
+	Arg1 interface{} `json:"arg1"`
+	Arg2 interface{} `json:"arg2"`
 }
 
 type Records struct {
@@ -18,13 +21,22 @@ type Records struct {
 	MX    map[string][]string
 	NS    map[string][]string
 	TXT   map[string][]string
+	CAA   map[string][]Record // save a full record
 }
 
-func getNames(recs map[string][]string) []string {
-	names := make([]string, 0, len(recs))
-	for name := range recs {
-		names = append(names, name)
+func getNames(recs interface{}) []string {
+	names := []string{}
+	switch recs := recs.(type) {
+	case map[string][]string:
+		for name := range recs {
+			names = append(names, name)
+		}
+	case map[string][]Record:
+		for name := range recs {
+			names = append(names, name)
+		}
 	}
+
 	// sort by pattern length; from long to short
 	sort.Slice(names, func(i, j int) bool {
 		return len(names[i]) > len(names[j])
@@ -52,6 +64,18 @@ func (rr *Records) LookupAAAA(domain string) []string {
 		}
 	}
 	return []string{}
+}
+
+func (rr *Records) LookupCAA(domain string) []Record {
+	domain = strings.ToLower(domain)
+	names := getNames(rr.CAA)
+
+	for _, name := range names {
+		if match(name, domain) {
+			return rr.CAA[name]
+		}
+	}
+	return []Record{}
 }
 func (rr *Records) LookupCNAME(domain string) string {
 	domain = strings.ToLower(domain)
